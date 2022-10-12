b194a0f41ea94cc8e0690ef3ffd7bdf7653ebd8f
Luis Lautaro Calvi <calvilautaro5@gmail.com>
2022-10-12 11:53:27 -0300

    modificaciones nuevas

--- Clase 14 Entrega Proyecto/api/carrito.js
+++ Clase 14 Entrega Proyecto/api/carrito.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 
 const { Productos } = require("./productos");
 
+
+const rutaCarrito = "./carritoList.txt";
+
 class Carrito{
     constructor(){
         this.producto = new Productos();
@@ -9,120 +12,79 @@ class Carrito{
         this.id = 1;
     }
 
-    async creandoCarrito (carr){
-        await fs.promises.writeFile("./carrito.txt", JSON.stringify(carr, null, 2), "utf-8");
-        return carr;
-    }
-    async getByIdCarrito(id){
+    async crearCarrito(){
         try{
-            const contenido = await this.getAllCarrito();
-            let carrito = contenido.find((carr) => carr.id == id);
-            return carrito;
-        }catch (error){
-            return {error: "No existen carritos"}
-        }
-
+        const carr = { id: this.id++, timeStamp: Date.now(), productos: [] };
+        this.carritos.push(carr);
+        
+        await fs.promises.writeFile(
+            rutaCarrito,
+            JSON.stringify(this.carritos, null, 2),
+            "utf-8"
+        )
+        return carr
+    } catch (e){
+        console.log(e)
+    }
     }
 
-    async getProd (id){
-        const carrProd = await this.getByIdCarrito(id);
-        console.log(carrProd.length);
+    async listarAll(){
+        try {
+            const contenido = await fs.promises.readFile(rutaCarrito, "utf-8");
+            const carritoList = JSON.parse(contenido);
+          
+            console.log(carritoList);
 
-        return carrProd.productos
-    }
+            return carritoList;
 
-    async getAllCarrito(){
-        try{
-            const contenido = await fs.promises.readFile("./carrito.txt", " utf-8");
-
-            return contenido.length ? JSON.parse(contenido) : {error: "No existen carritos"}
-        } catch (error){
-            return {error: "No existen carritos"}
-        }
-    }
-
-    async agregandoCarrito (){
-        try{
-            const contenido = await this.getAllCarrito();
-            const indice = contenido.sort((a, b) => b.id - a.id)[0].id;
-            const carr = {id: indice + 1, timeStamp: Date.now(), productos: [] };
-            contenido.push(carr);
-            this.creandoCarrito(contenido);
-
-            console.log ("Se ha creado un carrito")
-            return carr;
-        } catch (err){
-            console.log("hubo un problema")
+        } catch(error){
+            console.log(error);
         }
     }
 
-    async guardarProductoCarrito (idProd, idCarrito){
-        const prod = await this.producto.getById(idProd);
-        const carr = await this.getByIdCarrito(idCarrito);
-        console.log(carr.productos);
-        carr.productos.push(prod);
-
-        this.update(carr, idCarrito);
-
-        return{msj: "Producto agregado al carrito"};
+    listar(id){
+        let prod = this.carritos.find((carr) => carr.id == id);
+        return prod || { error: "carrito no encontrado" };
     }
 
 
-    async update (carr, id){
-        const contenido = await this.getAllCarrito();
-        let index = contenido.findIndex((p) => p.id == id);
-        carr.timeStamp = Date.now();
-
-        if(index >=0){
-            contenido.splice (index, 1, {...carr, id});
-            this.creandoCarrito(contenido);
-            return { msj: "Producto fue agregado"};
-        } else{
-            return {error: `Producto con id: ${carr.id} no existe`};
-        }
-
+    guardarProductoEnCarrito(idProd, idCarrito) {
+        console.log(idProd);
+        const producto = this.producto.getById(idProd);
+        this.carritos.forEach((carro) => {
+            carro.id == idCarrito ? carro.productos.push(producto) : null;
+        });
+        return this.carritos;
     }
 
-    async deleteByIdCarrito (id){
-        const contenido = await this.getAllCarrito();
-        let index = contenido.findIndex((carr) => carr.id == id);
-        contenido.splice (index, 1);
-
-        console.log(contenido);
-        this.creandoCarrito(contenido);
-        return {msj: `{ Carrito con id: ${id} fue eliminado }`};
+    actualizar(carr, id){
+        carr.id = Number(id);
+        let index = this.carritos.findIndex((carr) => carr.id == id);
+        this.productos.splice(index, 1, carr);
     }
 
-    async deleteProd (idProd, idCarrito){
-        const carrito = await this.getByIdCarrito(idCarrito);
-        console.log(carrito.productos);
-        if(carrito.productos.lenght){
-            for (var i = 0; i < carrito.productos.lenght; i++){
-                let obj = carrito.productos[i];
-                if (obj.id == idProd){
-                    let indexProducto = carrito.productos.findIndex ((prod) => prod.id == idProd);
-                    carrito.productos.splice(indexProducto, 1);
-                }            
-            }
-            this.update(carrito, idCarrito);
-            return {msj: `Producto con id: ${idProd} eliminado del carrito con id: ${idCarrito}`}
-        } else{
-            return {msj: "Producto no encontrado"}
-        }
+    borrar(id){
+        let index = this.carritos.findIndex((carr) => carr.id = id);
+        return this.carritos.splice(index, 1);
     }
-
 }
 
 module.exports = { Carrito };
 
 
 
-const carritoN = {
-    producto: "frutilla"
+const carritoN = [{
+    productos: "melon", 
 }
     
+]
+    
    
-
+const carritoNw= [{
+    productos: "melon", 
+}
+    
+]
 
 
 const carri = new Carrito;
@@ -131,6 +93,10 @@ async function call (){
 
 
 
+    await carri.crearCarrito(carritoN);
+    await carri.crearCarrito(carritoNw);   
+    await carri.guardarProductoEnCarrito(carritoN);   
 
-await carri.agregandoCarrito(carritoN);  }
+ }
 call();
+
